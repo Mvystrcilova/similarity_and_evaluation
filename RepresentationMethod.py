9a24192efbebe9ddaac8c8c6942286f62ce4b302
Michaela Vystrcilova <Mvystrcilova1@seznam.cz>
2019-03-24 11:31:34 +0100

    trenovani GRU spec s data generatorem

--- RepresentationMethod.py
+++ RepresentationMethod.py
@@ -417,8 +417,8 @@ class GRU_Spectrogram(AudioMethod):
 
         auto_encoder.compile(adam, loss='mse')
         encoder.compile(adam, loss='mse')
-        trainGen = generate_spectrograms(spec_directory=self.spec_directory, batch_size=79, mode="train")
-        auto_encoder.fit_generator(trainGen, steps_per_epoch=210, epochs=5)
+        trainGen = generate_spectrograms(spec_directory=self.spec_directory, batch_size=316, mode="train")
+        auto_encoder.fit_generator(trainGen, steps_per_epoch=52, epochs=5)
 
 
         # tbCallBack = keras.callbacks.TensorBoard(log_dir='~/evaluation_project/similarity_and_evaluation/Graph', histogram_freq=0,
