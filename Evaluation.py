c795870ccf2397b0f421ab63b6060aee1c4548fd
Michaela Vystrcilova <Mvystrcilova1@seznam.cz>
2019-04-03 21:57:07 +0200

    takovej cleanup

--- Evaluation.py
+++ Evaluation.py
@@ -136,47 +136,8 @@ class Evaluation():
 #     print(results.shape)
 #     results.to_csv(filename, sep=';', header=False, index=False)
 
-# for j in range(5):
-#     evaluation = Evaluation('distances/final_lstm_mel_distances.npy', 'useful_playlists', 'useful_songs')
-#     results = pandas.DataFrame(columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches', 'match_ranking', 'recall_at_10',
-#                  'recall_at_50', 'recall_at_100', 'nDGC'])
-#     i = 0
-#     for user in evaluation.users:
-#         user_results = evaluation.eval_playlist(user)
-#         print('user ', i, " out of ", len(evaluation.users))
-#         print(user_results)
-#         temp_frame = pandas.DataFrame([user_results],
-#                                       columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches',
-#                                                'match_ranking', 'recall_at_10', 'recall_at_50', 'recall_at_100',
-#                                                'nDGC'])
-#         results = results.append(temp_frame)
-#         i = i + 1
-#     filename = 'results/semi_old_lstm_mel_results/lstm_mel_' + str(j+1)
-#     print(results.shape)
-#     results.to_csv(filename, sep=';', header=False, index=False)
-#
-# for j in range(5):
-#     evaluation = Evaluation('distances/final_gru_mel_distances.npy', 'useful_playlists', 'useful_songs')
-#     results = pandas.DataFrame(
-#         columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches', 'match_ranking', 'recall_at_10',
-#                  'recall_at_50', 'recall_at_100', 'nDGC'])
-#     i = 0
-#     for user in evaluation.users:
-#         user_results = evaluation.eval_playlist(user)
-#         print('user ', i, " out of ", len(evaluation.users))
-#         print(user_results)
-#         temp_frame = pandas.DataFrame([user_results],
-#                                       columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches',
-#                                                'match_ranking', 'recall_at_10', 'recall_at_50', 'recall_at_100',
-#                                                'nDGC'])
-#         results = results.append(temp_frame)
-#         i = i + 1
-#     filename = 'results/semi_old_gru_mel_results/gru_mel_' + str(j + 1)
-#     print(results.shape)
-#     results.to_csv(filename, sep=';', header=False, index=False)
-
 for j in range(5):
-    evaluation = Evaluation('distances/pca_mel_distances_5712.npy', 'useful_playlists', 'useful_songs')
+    evaluation = Evaluation('mnt/0/lstm_mel_distances_5712.npy', 'useful_playlists', 'useful_songs')
     results = pandas.DataFrame(columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches', 'match_ranking', 'recall_at_10',
                  'recall_at_50', 'recall_at_100', 'nDGC'])
     i = 0
@@ -190,8 +151,47 @@ for j in range(5):
                                                'nDGC'])
         results = results.append(temp_frame)
         i = i + 1
-    filename = 'results/pca_mel_results_5715/pca_mel_' + str(j+1)
+    filename = 'mnt/0/results/lstm_mel_results_5712/lstm_mel_' + str(j+1)
     print(results.shape)
     results.to_csv(filename, sep=';', header=False, index=False)
+
+for j in range(5):
+    evaluation = Evaluation('mnt/0/gru_mel_distances_5712.npy', 'useful_playlists', 'useful_songs')
+    results = pandas.DataFrame(
+        columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches', 'match_ranking', 'recall_at_10',
+                 'recall_at_50', 'recall_at_100', 'nDGC'])
+    i = 0
+    for user in evaluation.users:
+        user_results = evaluation.eval_playlist(user)
+        print('user ', i, " out of ", len(evaluation.users))
+        print(user_results)
+        temp_frame = pandas.DataFrame([user_results],
+                                      columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches',
+                                               'match_ranking', 'recall_at_10', 'recall_at_50', 'recall_at_100',
+                                               'nDGC'])
+        results = results.append(temp_frame)
+        i = i + 1
+    filename = 'mnt/0/results/gru_mel_results_5712/gru_mel_' + str(j + 1)
+    print(results.shape)
+    results.to_csv(filename, sep=';', header=False, index=False)
+
+# for j in range(5):
+#     evaluation = Evaluation('distances/pca_mel_distances_5712.npy', 'useful_playlists', 'useful_songs')
+#     results = pandas.DataFrame(columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches', 'match_ranking', 'recall_at_10',
+#                  'recall_at_50', 'recall_at_100', 'nDGC'])
+#     i = 0
+#     for user in evaluation.users:
+#         user_results = evaluation.eval_playlist(user)
+#         print('user ', i, " out of ", len(evaluation.users))
+#         print(user_results)
+#         temp_frame = pandas.DataFrame([user_results],
+#                                       columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches',
+#                                                'match_ranking', 'recall_at_10', 'recall_at_50', 'recall_at_100',
+#                                                'nDGC'])
+#         results = results.append(temp_frame)
+#         i = i + 1
+#     filename = 'results/pca_mel_results_5715/pca_mel_' + str(j+1)
+#     print(results.shape)
+#     results.to_csv(filename, sep=';', header=False, index=False)
 
 # convert_files_to_mfcc('not_empty_songs', 320)
