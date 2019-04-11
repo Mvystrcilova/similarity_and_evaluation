eb46755f62af33553ce9164770de560897ffa6e3
Michaela Vystrcilova <Mvystrcilova1@seznam.cz>
2019-04-11 23:52:33 +0200

    reprezentace gru spec

--- Evaluation.py
+++ Evaluation.py
@@ -151,7 +151,43 @@ class Evaluation():
 #     results.to_csv(filename, sep=';', header=False, index=False)
 
 
+for j in range(5):
+    evaluation = Evaluation('mnt/0/distances/final_lstm_spec_distances.npy', 'mnt/0/useful_playlists', 'mnt/0/useful_songs', True)
+    results = pandas.DataFrame(columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches', 'match_ranking', 'recall_at_10',
+                 'recall_at_50', 'recall_at_100', 'nDGC'])
+    i = 0
+    for user in evaluation.users:
+        user_results = evaluation.eval_playlist(user)
+        print('user ', i, " out of ", len(evaluation.users))
+        print(user_results)
+        temp_frame = pandas.DataFrame([user_results],
+                                      columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches',
+                                               'match_ranking', 'recall_at_10', 'recall_at_50', 'recall_at_100',
+                                               'nDGC'])
+        results = results.append(temp_frame)
+        i = i + 1
+    filename = 'mnt/0/results/all_lstm_spec_results/lstm_spec_' + str(j+1)
+    print(results.shape)
+    results.to_csv(filename, sep=';', header=False, index=False)
 
+for j in range(5):
+    evaluation = Evaluation('mnt/0/distances/final_lstm_spec_distances.npy', 'mnt/0/useful_playlists', 'mnt/0/useful_songs', False)
+    results = pandas.DataFrame(columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches', 'match_ranking', 'recall_at_10',
+                 'recall_at_50', 'recall_at_100', 'nDGC'])
+    i = 0
+    for user in evaluation.users:
+        user_results = evaluation.eval_playlist(user)
+        print('user ', i, " out of ", len(evaluation.users))
+        print(user_results)
+        temp_frame = pandas.DataFrame([user_results],
+                                      columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches',
+                                               'match_ranking', 'recall_at_10', 'recall_at_50', 'recall_at_100',
+                                               'nDGC'])
+        results = results.append(temp_frame)
+        i = i + 1
+    filename = 'mnt/0/results/lstm_spec_results/lstm_spec_' + str(j+1)
+    print(results.shape)
+    results.to_csv(filename, sep=';', header=False, index=False)
 
 
 # convert_files_to_mfcc('not_empty_songs', 320)
