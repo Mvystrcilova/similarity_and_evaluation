bf9103f5602cf2d5e14468aea26a40494aa01551
Michaela Vystrcilova <Mvystrcilova1@seznam.cz>
2019-04-16 22:17:32 +0200

    reprezentace gru speckd

--- evaluate_gru_spec.py
+++ evaluate_gru_spec.py
@@ -3,7 +3,7 @@ from Evaluation import Evaluation
 
 
 for j in range(5):
-    evaluation = Evaluation('mnt/0/gru_spec_distances.npy', '/mnt/0/useful_playlists', 'mnt/0/useful_songs', False, threshold=0.9997426710857443)
+    evaluation = Evaluation('mnt/0/distances/final_gru_spec_distances.npy', '/mnt/0/useful_playlists', 'mnt/0/useful_songs', False, threshold=0.9997426710857443)
     results = pandas.DataFrame(columns=['playlist_lenght', 'test_list_lenght', 'number_of_matches', 'match_ranking', 'recall_at_10',
                  'recall_at_50', 'recall_at_100', 'nDGC'])
     i = 0
