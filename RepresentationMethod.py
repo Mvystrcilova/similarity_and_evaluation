7687181b6679bef4674c7c8021be2a4ad0ade4d1
Michaela Vystrcilova <Mvystrcilova1@seznam.cz>
2019-11-16 16:13:18 +0100

    training 30sec neural networks on server

--- RepresentationMethod.py
+++ RepresentationMethod.py
@@ -799,13 +799,13 @@ def generate_spectrograms(spec_directory, batch_size, mode='train'):
 #
 #
 # # Volta 02
-# gru_mel = GRU_Mel_Spectrogram(11)
-# gru_mel.train([])
+gru_mel = GRU_Mel_Spectrogram(11)
+gru_mel.train([])
 #
 # #
 #VOlta 3
-lstm_mel = LSTM_Mel_Spectrogram(2)
-lstm_mel.train([])
+# lstm_mel = LSTM_Mel_Spectrogram(2)
+# lstm_mel.train([])
 
 # #
 # try:
